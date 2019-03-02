9e43cc3830d66b762ae5fc07c640a88bdc2f8a96
ManicalEngineer <Unhfireboy@yahoo.com>
2019-03-01 20:35:51 -0500

    Update main pg

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Service, Information
+
 # Register your models here.
 admin.site.register(Service)
 admin.site.register(Information)
--- base/views.py
+++ base/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Service, Information
+from albumns.models import Cover, Album
 
 # Create your views here.
 def index(request):
     infos = Information.objects.all()
     info = infos[0]
     services = Service.objects.all()
-    return render(request, 'base/index.html', {'info': info, 'services': services})
+    albums = Album.objects.all()
+    return render(request, 'base/index.html', {'info': info, 'services': services, 'albums': albums})
